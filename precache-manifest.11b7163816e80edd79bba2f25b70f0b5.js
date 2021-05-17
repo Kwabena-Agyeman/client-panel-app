b0ea2275e11f20dcebabb294dc38d776504a4cf4
Kwabena Agyeman <kagyeman@ymail.com>
2021-05-17 20:36:16 +0000

    Updates

---
+++ precache-manifest.11b7163816e80edd79bba2f25b70f0b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ea5b034e55388e5a35d",
+    "url": "/client-panel-app/static/css/main.aa532d3c.chunk.css"
+  },
+  {
+    "revision": "0ea5b034e55388e5a35d",
+    "url": "/client-panel-app/static/js/main.288744d3.chunk.js"
+  },
+  {
+    "revision": "53ca25a614a9a3b9bb11",
+    "url": "/client-panel-app/static/js/runtime~main.8fdf5092.js"
+  },
+  {
+    "revision": "443c77270d88c4a66696",
+    "url": "/client-panel-app/static/js/2.542b6762.chunk.js"
+  },
+  {
+    "revision": "909f4d4d0c59b02dcaa6",
+    "url": "/client-panel-app/static/js/3.2740c647.chunk.js"
+  },
+  {
+    "revision": "e3358c51dc836e988b1968b5226d0498",
+    "url": "/client-panel-app/static/media/clients.e3358c51.jpeg"
+  },
+  {
+    "revision": "aa2f359d7419d564f54531a0618a6ac8",
+    "url": "/client-panel-app/index.html"
+  }
+];
